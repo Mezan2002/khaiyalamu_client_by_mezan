683553fbc706ad878a3aceaad4aea8c6609d4c04
Mezan2002 <mezanurr009@gmail.com>
2022-11-09 01:08:18 +0600

    Place Review Card Added

---
+++ src/Pages/SingleServiceCard/PlaceReview.js
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
+
+const PlaceReview = ({ handleSubmitReview }) => {
+  const { user } = useContext(AuthContext);
+  return (
+    <div className="card-body">
+      <h2 className="text-4xl font-bold">
+        Leave a feedback about this service
+      </h2>
+      <div className="mt-5">
+        <form onSubmit={handleSubmitReview}>
+          <textarea
+            className="textarea textarea-bordered w-full h-48"
+            placeholder="Write a review *"
+            required
+            name="review"
+          ></textarea>
+          <input
+            type="text"
+            placeholder="Your Name *"
+            className="input input-bordered w-full mt-2"
+            required
+            defaultValue={user.displayName}
+            name="username"
+          />
+          <input
+            type="text"
+            placeholder="Your Email *"
+            className="input input-bordered w-full mt-2"
+            required
+            defaultValue={user.email}
+            name="useremail"
+          />
+          <input
+            type="text"
+            placeholder="Rate Us in 5 *"
+            className="input input-bordered w-full mt-2"
+            required
+            name="ratings"
+          />
+          <input className="btn mt-10 btn-block" type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceReview;
--- src/Pages/SingleServiceCard/SingleServiceCard.js
+++ src/Pages/SingleServiceCard/SingleServiceCard.js
@@ -3,6 +3,8 @@ import toast from "react-hot-toast";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
 import { FaCalendar, FaStar } from "react-icons/fa";
+import ReviewCard from "./ReviewCard";
+import PlaceReview from "./PlaceReview";
 
 const SingleServiceCard = () => {
   const { user } = useContext(AuthContext);
@@ -67,125 +69,13 @@ const SingleServiceCard = () => {
       </div>
       <div className="mt-10">
         <div className="card  bg-base-100 shadow-xl">
-          <div className="p-10">
-            <div className="">
-              <div className="flex items-center mb-5">
-                <div>
-                  <img
-                    className="w-14 rounded-full mr-4"
-                    src={user?.photoURL}
-                    alt=""
-                  />
-                </div>
-                <div className="">
-                  <div className="flex">
-                    <p>
-                      by :{" "}
-                      <span className="font-bold">{user?.displayName}</span>
-                    </p>
-                    <div className="flex items-center ml-2">
-                      <p>
-                        <FaStar className="text-yellow-400"></FaStar>
-                      </p>
-                      <p>
-                        <FaStar className="text-yellow-400"></FaStar>
-                      </p>
-                      <p>
-                        <FaStar className="text-yellow-400"></FaStar>
-                      </p>
-                      <p>
-                        <FaStar className="text-yellow-400"></FaStar>
-                      </p>
-                      <p>
-                        <FaStar className="text-yellow-400"></FaStar>
-                      </p>
-                    </div>
-                  </div>
-                  <p className="flex items-center">
-                    <FaCalendar className="mr-2 text-orange-500"></FaCalendar>
-                    December 14, 2022 | 12.23 pm
-                  </p>
-                </div>
-              </div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-                odit eaque corporis quibusdam quos illo natus excepturi omnis
-                repudiandae earum! Perferendis, similique? Repellat, iusto
-                ducimus doloribus velit rerum quibusdam dignissimos veritatis
-                vero fuga nihil fugit recusandae voluptatem, illum alias
-                inventore deserunt eligendi voluptas repellendus tempore nulla
-                cumque. Recusandae, inventore excepturi totam quam
-                necessitatibus deleniti maiores, eveniet dolores, quas officiis
-                tenetur. Reprehenderit cupiditate natus, error quaerat
-                doloremque nulla ea dolore voluptatibus. Ex adipisci ratione
-                quas magni, excepturi atque aspernatur eligendi ullam doloremque
-                cumque dicta cum recusandae neque maiores fugiat eius distinctio
-                dolor repellat! Iusto itaque temporibus similique, voluptatem
-                aliquid facere quod consectetur maiores libero voluptas quasi
-                eveniet et aspernatur ad quo at odit velit natus veniam
-                blanditiis laborum voluptatum qui? Sapiente delectus sint non.
-                Architecto distinctio modi pariatur animi harum, dolorem
-                obcaecati sed quos id ipsa magni inventore error. Consequatur
-                perspiciatis culpa eligendi at eveniet, animi provident
-                perferendis, libero nam numquam voluptatum id labore voluptatem
-                tempore omnis magni? Quis quasi officia neque praesentium ab
-                architecto perspiciatis obcaecati dolorem minima aspernatur
-                ipsam provident quisquam voluptatum blanditiis voluptatem nemo
-                qui ut accusantium in aliquam, nihil fugit illum? Suscipit
-                dignissimos, asperiores porro inventore doloribus eveniet ut a!
-                Molestiae velit exercitationem aspernatur maxime voluptatum
-                unde.
-              </p>
-            </div>
-          </div>
+          <ReviewCard></ReviewCard>
         </div>
       </div>
       <div>
         <div className="card mt-10 shadow-xl">
           {user?.uid ? (
-            <div className="card-body">
-              <h2 className="text-4xl font-bold">
-                Leave a feedback about this service
-              </h2>
-              <div className="mt-5">
-                <form onSubmit={handleSubmitReview}>
-                  <textarea
-                    className="textarea textarea-bordered w-full h-48"
-                    placeholder="Write a review *"
-                    required
-                    name="review"
-                  ></textarea>
-                  <input
-                    type="text"
-                    placeholder="Your Name *"
-                    className="input input-bordered w-full mt-2"
-                    required
-                    defaultValue={user.displayName}
-                    name="username"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Your Email *"
-                    className="input input-bordered w-full mt-2"
-                    required
-                    defaultValue={user.email}
-                    name="useremail"
-                  />
-                  <input
-                    type="text"
-                    placeholder="Rate Us in 5 *"
-                    className="input input-bordered w-full mt-2"
-                    required
-                    name="ratings"
-                  />
-                  <input
-                    className="btn mt-10 btn-block"
-                    type="submit"
-                    value="Submit"
-                  />
-                </form>
-              </div>
-            </div>
+            <PlaceReview handleSubmitReview={handleSubmitReview}></PlaceReview>
           ) : (
             <div className="py-20">
               <h2 className="text-center text-4xl capitalize">
