62ea44de9b30dca1b8bf26e18db153c178412a50
Mezan2002 <mezanurr009@gmail.com>
2022-11-08 17:16:27 +0600

    Service Details Added

--- src/Pages/SingleServiceCard/SingleServiceCard.js
+++ src/Pages/SingleServiceCard/SingleServiceCard.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const SingleServiceCard = () => {
+  const service = useLoaderData();
+  const { name, image, price, deliveryCharge, ratings, description } = service;
   return (
-    <div>
-      <h2>hello</h2>
+    <div className="mb-20">
+      <div className="card shadow-xl p-5">
+        <figure>
+          <img
+            className="rounded-2xl border border-gray-500"
+            src={image}
+            alt=""
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="text-5xl font-bold mb-10">{name}</h2>
+          <div className="font-semibold flex">
+            <span className="border-r-2 border-gray-400 pr-5">
+              Price: {price} BDT
+            </span>
+            <span className="border-r-2 border-gray-400 px-5">
+              Delivery Charge: {deliveryCharge} BDT
+            </span>
+            <span className="border-r-2 border-gray-400 px-5">
+              Ratings: {ratings}{" "}
+            </span>
+          </div>
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 };
