e1bf42f4322437c5d20d22a230bd958abe450bea
Mezan2002 <mezanurr009@gmail.com>
2022-11-08 15:02:51 +0600

    Description added by conditional rendering

--- src/Pages/Home/ServiceCard/ServiceCard.js
+++ src/Pages/Home/ServiceCard/ServiceCard.js
@@ -1,20 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
-  const { name, image, price, deliveryCharge, ratings } = service;
+  const { name, image, price, deliveryCharge, ratings, description } = service;
   return (
     <div>
       <div className="card w-96 shadow-xl">
         <figure>
-          <img className="w-11/12 rounded-2xl h-64" src={image} alt="Shoes" />
+          <img
+            className="w-11/12 rounded-2xl h-64 border border-gray-500"
+            src={image}
+            alt="Shoes"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
-          <p>Price: BDT{price}</p>
-          <p>Delivery Charge: BDT{deliveryCharge}</p>
-          <p>Ratings: {ratings} </p>
+          <div>
+            {description.length > 100 ? (
+              <p>
+                {description.slice(0, 100)}...{" "}
+                <>
+                  <Link className="text-blue-800">see more</Link>
+                </>
+              </p>
+            ) : (
+              <p>{description}</p>
+            )}
+          </div>
+          <div className="font-semibold">
+            <p>Price: {price} BDT</p>
+            <p>Delivery Charge: {deliveryCharge} BDT</p>
+            <p>Ratings: {ratings} </p>
+          </div>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">View Details</button>
           </div>
         </div>
       </div>
